40d39cb862f48f527a3281d551a68ab7359a86b1
Olha <olha.idf@gmail.com>
2020-09-10 10:08:26 +0200

    creation de db et table

---
+++ src/Entity/Projets.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjetsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjetsRepository::class)
+ */
+class Projets
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_site;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $outil;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomSite(): ?string
+    {
+        return $this->nom_site;
+    }
+
+    public function setNomSite(string $nom_site): self
+    {
+        $this->nom_site = $nom_site;
+
+        return $this;
+    }
+
+    public function getOutil(): ?string
+    {
+        return $this->outil;
+    }
+
+    public function setOutil(string $outil): self
+    {
+        $this->outil = $outil;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): self
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
