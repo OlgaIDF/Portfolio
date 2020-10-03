e5338dc38526b74ab96dd7c8cab0b0d5a95d7ae4
Olha <olha.idf@gmail.com>
2020-10-03 17:41:01 +0200

    Rollerworks

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,27 +4,35 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Rollerworks\Component\PasswordStrength\Validator\Constraints\PasswordStrength;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'placeholder' => 'test.test@mail.com'
+                ]
+            ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
-                        'message' => 'You should agree to our terms.',
+                        'message' => 'Vous devez accepter nos conditions générales d\'utilisation',
                     ]),
                 ],
+                'label' => 'Accepter les conditions générales d\'utilisation'
             ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
@@ -32,16 +40,29 @@ class RegistrationFormType extends AbstractType
                 'mapped' => false,
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Merci de renseigner un mot de passe',
                     ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+                    // new Length([
+                    //     'min' => 8,
+                    //     'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
+                    //     // max length allowed by Symfony for security reasons
+                    //     'max' => 4096,
+                    // ]),
+                    new PasswordStrength([
+                        // longueur mini
+                        'minLength' => 8,
+                        'tooShortMessage' => 'Le mot de passe doit contenir au moins 8 caractères',
+                        // force mini
+                        'minStrength' => 4,
+                        'message' => 'Le mot de passe doit contenir au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spécial'
+                    ])
                 ],
+                'attr' => [
+                    'placeholder' => 'mot de passe'
+                ],
+                'label' => 'Mot de passe'
             ])
+           
         ;
     }
 
